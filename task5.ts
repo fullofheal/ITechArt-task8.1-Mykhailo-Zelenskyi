aebe8bbde6c43cd208c645eebfa2d9ab934c54e3
fullofheal <zelenskiymisha@gmail.com>
2023-02-01 16:54:48 +0200

    task 5 updated

--- task5.ts
+++ task5.ts
@@ -1,8 +1,16 @@
-type FIXME = any
+// NOT FINISHED YET
+// type FIXME = any
 
 type OrderState = 'initial' | 'inWork' | 'buyingSupplies' | 'producing' | 'fullfilled'
 
+// type FilteredStates = 'initial' | 'inWork' | 'fullfilled'
+
+function filteredStates(state: string): state is ('initial' | 'inWork' | 'fullfilled') {
+    return typeof state === "string";
+}
+
 // Hint: with type guards return type can be removed
-export const getUserOrderStates = (orderStates: OrderState[]):Omit<OrderState[], 'buyingSupplies' | 'producing'> => {
-    return orderStates.filter((state) => state !== 'buyingSupplies' && state !== 'producing')
+export const getUserOrderStates = (orderStates: OrderState[]) => {
+    
+    return orderStates.filter(filteredStates)
 }
